9b75068ba618c140e7fc21fada60c7030aebb200
phanindra2705 <phanindranaidug@gmail.com>
2024-12-19 13:34:56 -0600

    first commit

---
+++ If.py
@@ -0,0 +1,36 @@
+# num = input('enter a num ')
+#
+# if int(num) % 2 == 0:
+#     print('num is even')
+# else:
+#     print('num is odd')
+
+
+
+# temp=input('enter the num ')
+#
+# if int(temp)>30:
+#     print('hot day')
+# elif int(temp)<10:
+#     print('cold day')
+# else:
+#     print('Its neither hot nor cold')
+
+# name = input('enter name')
+
+# if len(name)<3:
+#     print("Name must be atleast 3 characters")
+# elif len(name)>50:
+#     print('name can be max of 50 characters')
+# else:
+#     print('name looks good')
+
+weight = int(input('weight'))
+
+unit = input('Lbs or Kgs ')
+
+
+if unit == 'L':
+    print(weight * 0.45)
+elif unit=='K':
+    print(weight * 2.2)
