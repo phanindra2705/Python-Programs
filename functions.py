2acbef2aac31dc261111ffdc155e013ecfb0cb29
phanindra2705 <phanindranaidug@gmail.com>
2024-12-23 12:03:42 -0600

    commiting functions python program

---
+++ functions.py
@@ -0,0 +1,14 @@
+def emoji_convertor(message):
+    words = message.split(' ')
+    emojis = {
+        ":)": "😃",
+        ":(": "🙃"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+
+message = input("> ")
+print(emoji_convertor(message))
