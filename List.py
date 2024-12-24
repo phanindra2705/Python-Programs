007536de01e0be58da0bdd11eb8683da33ea3c91
phanindra2705 <phanindranaidug@gmail.com>
2024-12-24 01:04:35 -0600

    Commiting list program

---
+++ List.py
@@ -0,0 +1,17 @@
+# nums = [10,443,23,4,21]
+#
+# # print(max(nums))
+# max = nums[0]
+#
+# for num in nums:
+#     if num > max:
+#         max = num
+# print(max)
+
+# nums = [32,23,4,5,4,5]
+#
+# dup = nums[0]
+#
+# for num in nums:
+#     if dup == num:
+
