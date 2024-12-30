1af6ed488a54e1c5eddc51ccb70e47c4f46304b2
phanindra2705 <phanindranaidug@gmail.com>
2024-12-30 10:05:12 -0600

    Commiting prime number program

---
+++ PrimeNumber.py
@@ -0,0 +1,15 @@
+# Check if a number is prime
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(f"{num} is a prime number!")
+else:
+    print(f"{num} is not a prime number.")
